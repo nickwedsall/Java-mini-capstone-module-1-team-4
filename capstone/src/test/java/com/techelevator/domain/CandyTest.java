69d6c64f6ed83decffcb132451f6c849b2fb40a9
Dan Shaw <danielmathershaw@gmail.com>
2022-02-14 10:30:57 -0500

    Added CandyTest constructor test

--- capstone/src/test/java/com/techelevator/domain/CandyTest.java
+++ capstone/src/test/java/com/techelevator/domain/CandyTest.java
@@ -1,4 +1,23 @@
 package com.techelevator.domain;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CandyTest {
+
+    @Test
+    public void Candy_constructor_gives_correct_instance_variable_assignment() {
+        // Arrange
+        Candy testCandy = new Candy("testCandyName", 1.00);
+        String expectedName = "testCandyName";
+        double expectedPrice = 1.00;
+
+        // Act
+        String actualName = testCandy.getItemName();
+        double actualPrice = testCandy.getPrice();
+
+        // Assert
+        Assert.assertEquals("String itemName in constructor DID NOT assign name properly", expectedName, actualName);
+        Assert.assertEquals("double price in constructor DID NOT assign price properly", expectedPrice, actualPrice, 0.0);
+    }
 }
