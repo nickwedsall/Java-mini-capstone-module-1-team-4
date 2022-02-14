58c99c24921c4a4ecd49b7f2ae46e03b42b3cd08
Kevin Steinbach <kevin.steinbach86@gmail.com>
2022-02-14 14:20:52 -0500

    Refactored CandyTest

--- capstone/src/test/java/com/techelevator/domain/CandyTest.java
+++ capstone/src/test/java/com/techelevator/domain/CandyTest.java
@@ -1,14 +1,21 @@
 package com.techelevator.domain;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class CandyTest {
+import java.sql.SQLClientInfoException;
 
+public class CandyTest {
+    private Candy testCandy;
+
+    @Before
+    public void Candy() {
+        this.testCandy = new Candy("testCandyName", 1.00);
+    }
     @Test
     public void Candy_constructor_gives_correct_instance_variable_assignment() {
         // Arrange
-        Candy testCandy = new Candy("testCandyName", 1.00);
         String expectedName = "testCandyName";
         double expectedPrice = 1.00;
 
@@ -20,4 +27,32 @@ public class CandyTest {
         Assert.assertEquals("String itemName in constructor DID NOT assign name properly", expectedName, actualName);
         Assert.assertEquals("double price in constructor DID NOT assign price properly", expectedPrice, actualPrice, 0.0);
     }
+
+    @Test
+    public void getDispenseMessage_returns_message_munch_munch() {
+        //Arrange
+        String expectedString = "Munch Munch, Yum!";
+
+        //Act
+        String actualString = testCandy.getDispenseMessage();
+
+        //Assert
+        Assert.assertEquals("getDispenseMessage() object does not return \"Munch Munch, Yum!\"", expectedString, actualString);
+
+    }
+
+    @Test
+    public void toString_returns_correct_formatted_strings() {
+        //Arrange
+        String expected = "testCandyName $1.00";
+
+        //Act
+        String actual = testCandy.toString();
+
+        //Assert
+        Assert.assertEquals("toString() did not return \"testCandyName $1.00\"", expected, actual);
+
+
+    }
+
 }
